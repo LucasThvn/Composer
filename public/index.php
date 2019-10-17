04965a10e9ca702d717155ea86950a08c79ae4a3
Lucas Thauvin <lucas@MacBook-Pro-de-Lucas.local>
2019-10-17 17:57:25 +0200

    Quete composer finie

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use HelloWorld\SayHello;
+use \App\Wcs\Hello;
+
+$Hello = new SayHello();
+$Hello2 = new Hello();
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <p><?= $Hello->world() ?></p>
+    <p><?= Hello::talk() ?></p>
+</body>
+</html>
